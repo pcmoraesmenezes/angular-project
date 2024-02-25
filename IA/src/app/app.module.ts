4f617a7a9abbaf06b97eb05f1020283e2e0b4127
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-02-25 20:07:41 -0300

    :sparkles: feat: Importando o cliente http

--- IA/src/app/app.module.ts
+++ IA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './layout/nav/nav.component';
