91864a134bec41c9ae82c1da664488d165c9f071
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-02-25 20:08:37 -0300

    :sparkles: feat: Adicionando componente para contato e obtendo umget de teste para futura logo da pagina

--- IA/src/app/layout/footer/footer.component.ts
+++ IA/src/app/layout/footer/footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.less'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
+  contact = 
+    {
+      email: 'paulo@email.com',
+      photo: ''
+    }
+
+
+  constructor(private http: HttpClient) { }
+
+   ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/photos')
+    .subscribe((response:any) => {
+      this.contact.photo = response[0].url;
+    })
+}
 }
